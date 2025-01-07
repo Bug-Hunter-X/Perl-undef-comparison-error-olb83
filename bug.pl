6639f2ee8b64f5758a430d06dbc9fe895f457e2d
BugHunter-X <askzenai@gmail.com>
2025-01-07 11:28:04 +0100

    Add bug.pl file

---
+++ bug.pl
@@ -0,0 +1,7 @@
+my $var = undef; #Declare an undefined scalar variable.
+
+if ($var eq undef) { #Attempt to compare the variable with undef directly
+    print "Variable is undef\n";
+} else {
+    print "Variable is defined\n";
+}
